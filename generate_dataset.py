739f932f569da302f2c63774a06f72aba63747e6
RobbieJennings <robbie.jennings97@gmail.com>
2019-03-12 16:27:42 +0000

    Fixed Train and Test generation

--- generate_dataset.py
+++ generate_dataset.py
@@ -3,7 +3,7 @@ import os
 import cv2
 
 
-def build(dataset, test_portion):
+def build(dataset, train_portion):
     root = os.path.dirname(os.path.abspath(__file__))
     path = root + "/" + dataset + "/"
     directory = os.listdir(path)
@@ -14,9 +14,9 @@ def build(dataset, test_portion):
         image = cv2.imread(path + file, 0)
         images.append(image)
 
-    split_point = int(len(images) * test_portion)
+    split_point = int(len(images) * train_portion)
 
-    x_test = np.array(images[:split_point])
-    x_train = np.array(images[split_point:])
+    x_train = np.array(images[:split_point])
+    x_test = np.array(images[split_point:])
 
-    np.savez(dataset, x_test, x_train)
+    np.savez(dataset, x_train, x_test)
