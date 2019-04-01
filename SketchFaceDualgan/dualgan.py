b4a77458fca1703c33b0f7eaf2c31bc26c4bdf3f
RobbieJennings <robbie.jennings97@gmail.com>
2019-04-01 20:54:30 +0100

    Divided input into train/test for gan and dualgan

--- SketchFaceDualgan/dualgan.py
+++ SketchFaceDualgan/dualgan.py
@@ -1,8 +1,4 @@
 from __future__ import print_function, division
-import scipy
-
-import os
-import dataset
 
 from keras.preprocessing import image
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
@@ -14,16 +10,17 @@ from keras.optimizers import RMSprop, Adam
 from keras.utils import to_categorical
 import keras.backend as K
 
-import matplotlib.pyplot as plt
-
 import numpy as np
+import os
+import dataset
 
-class DUALGAN():
+
+class DUALGAN:
     def __init__(self):
         self.img_rows = 192
         self.img_cols = 128
         self.channels = 1
-        self.img_dim = self.img_rows*self.img_cols
+        self.img_dim = self.img_rows * self.img_cols
 
         optimizer = Adam(0.0002, 0.5)
 
@@ -37,10 +34,10 @@ class DUALGAN():
             optimizer=optimizer,
             metrics=['accuracy'])
 
-        #-------------------------
+        # -------------------------
         # Construct Computational
         #   Graph of Generators
-        #-------------------------
+        # -------------------------
 
         # Build the generators
         self.G_AB = self.build_generator()
@@ -120,21 +117,25 @@ class DUALGAN():
         return K.mean(y_true * y_pred)
 
     def train(self, epochs, batch_size=128, sample_interval=50):
+        # Generate the dataset
+        dataset.generate("sketches", self.img_cols, self.img_rows, 0.9)
+        dataset.generate("pictures", self.img_cols, self.img_rows, 0.9)
 
-        # Generate the datasets
-        dataset.generate("sketches", self.img_cols, self.img_rows, 1)
-        dataset.generate("pictures", self.img_cols, self.img_rows, 1)
-
-        # Load the datasets
-        (X_A, _) = dataset.load("sketches")
-        (X_B, _) = dataset.load("pictures")
+        # Load the dataset
+        (A_train, A_test) = dataset.load("sketches")
+        (B_train, B_test) = dataset.load("pictures")
 
         # Rescale -1 to 1
-        X_A = (X_A.astype(np.float32) - 127.5) / 127.5
-        X_B = (X_B.astype(np.float32) - 127.5) / 127.5
+        A_train = (A_train.astype(np.float32) - 127.5) / 127.5
+        B_train = (B_train.astype(np.float32) - 127.5) / 127.5
+        A_test = (A_test.astype(np.float32) - 127.5) / 127.5
+        B_test = (B_test.astype(np.float32) - 127.5) / 127.5
 
-        X_A = X_A.reshape(X_A.shape[0], self.img_dim)
-        X_B = X_B.reshape(X_B.shape[0], self.img_dim)
+        # Reshape for generators / discriminators
+        A_train = A_train.reshape(A_train.shape[0], self.img_dim)
+        B_train = B_train.reshape(B_train.shape[0], self.img_dim)
+        A_test = A_test.reshape(A_test.shape[0], self.img_dim)
+        B_test = B_test.reshape(B_test.shape[0], self.img_dim)
 
         clip_value = 0.01
         n_critic = 4
@@ -153,8 +154,8 @@ class DUALGAN():
                 # ----------------------
 
                 # Sample generator inputs
-                imgs_A = self.sample_generator_input(X_A, batch_size)
-                imgs_B = self.sample_generator_input(X_B, batch_size)
+                imgs_A = self.sample_generator_input(A_train, batch_size)
+                imgs_B = self.sample_generator_input(B_train, batch_size)
 
                 # Translate images to their opposite domain
                 fake_B = self.G_AB.predict(imgs_A)
@@ -190,14 +191,17 @@ class DUALGAN():
 
             # If at save interval => save generated image samples
             if epoch % sample_interval == 0:
-                self.save_imgs(epoch, X_A, X_B)
+                self.save_imgs(epoch, A_test, B_test)
 
-    def save_imgs(self, epoch, X_A, X_B):
-        sample_size = 4
+    def save_imgs(self, epoch, A_test, B_test):
+        sample_size = 32
 
         # Sample generator inputs
-        imgs_A = self.sample_generator_input(X_A, sample_size)
-        imgs_B = self.sample_generator_input(X_B, sample_size)
+        # imgs_A = self.sample_generator_input(A_test, sample_size)
+        # imgs_B = self.sample_generator_input(B_test, sample_size)
+
+        imgs_A = A_test[0:sample_size]
+        imgs_B = B_test[0:sample_size]
 
         # Images translated to their opposite domain
         fake_B = self.G_AB.predict(imgs_A)
@@ -210,34 +214,25 @@ class DUALGAN():
         fake_B = 0.5 * fake_B + 0.5
 
         # Reshape images
-        imgs_A = imgs_A.reshape((sample_size, self.img_rows, self.img_cols, 1))
-        imgs_B = imgs_B.reshape((sample_size, self.img_rows, self.img_cols, 1))
-        fake_A = fake_A.reshape((sample_size, self.img_rows, self.img_cols, 1))
-        fake_B = fake_B.reshape((sample_size, self.img_rows, self.img_cols, 1))
+        imgs_A = imgs_A.reshape((sample_size, self.img_rows, self.img_cols, self.channels))
+        imgs_B = imgs_B.reshape((sample_size, self.img_rows, self.img_cols, self.channels))
+        fake_A = fake_A.reshape((sample_size, self.img_rows, self.img_cols, self.channels))
+        fake_B = fake_B.reshape((sample_size, self.img_rows, self.img_cols, self.channels))
 
-        if not os.path.exists('dualgan_results'):
-            os.makedirs('dualgan_results')
-
-        if not os.path.exists('dualgan_results/sketch2face_input'):
-            os.makedirs('dualgan_results/sketch2face_input')
+        # Make directories
         if not os.path.exists('dualgan_results/sketch2face_input/' + str(epoch)):
             os.makedirs('dualgan_results/sketch2face_input/' + str(epoch))
-
-        if not os.path.exists('dualgan_results/sketch2face_output'):
-            os.makedirs('dualgan_results/sketch2face_output')
+            
         if not os.path.exists('dualgan_results/sketch2face_output/' + str(epoch)):
             os.makedirs('dualgan_results/sketch2face_output/' + str(epoch))
-
-        if not os.path.exists('dualgan_results/face2sketch_input'):
-            os.makedirs('dualgan_results/face2sketch_input')
+            
         if not os.path.exists('dualgan_results/face2sketch_input/' + str(epoch)):
             os.makedirs('dualgan_results/face2sketch_input/' + str(epoch))
-
-        if not os.path.exists('dualgan_results/face2sketch_output'):
-            os.makedirs('dualgan_results/face2sketch_output')
+            
         if not os.path.exists('dualgan_results/face2sketch_output/' + str(epoch)):
             os.makedirs('dualgan_results/face2sketch_output/' + str(epoch))
 
+        # Save images
         for i in range(len(imgs_A)):
             img = image.array_to_img(imgs_A[i])
             img.save('dualgan_results/sketch2face_input/' + str(epoch) + '/' + str(i) + '.png')
@@ -257,4 +252,4 @@ class DUALGAN():
 
 if __name__ == '__main__':
     gan = DUALGAN()
-    gan.train(epochs=30000, batch_size=32, sample_interval=200)
+    gan.train(epochs=100000, batch_size=32, sample_interval=1000)
--- SketchFaceDualgan/gan.py
+++ SketchFaceDualgan/gan.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, division
 
-import generate_dataset
-import load_dataset
+from keras.preprocessing import image
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
@@ -9,19 +8,18 @@ from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
 
-import matplotlib.pyplot as plt
-
-import sys
-
 import numpy as np
+import dataset
+import os
 
-class GAN():
+
+class GAN:
     def __init__(self):
-        self.img_rows = 80
-        self.img_cols = 64
+        self.img_rows = 192
+        self.img_cols = 128
         self.channels = 1
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
-        self.latent_dim = 100
+        self.img_size = self.img_rows * self.img_cols * self.channels
 
         optimizer = Adam(0.0002, 0.5)
 
@@ -34,8 +32,8 @@ class GAN():
         # Build the generator
         self.generator = self.build_generator()
 
-        # The generator takes noise as input and generates imgs
-        z = Input(shape=(self.latent_dim,))
+        # The generator takes sketches as input and generates pictures
+        z = Input(shape=(self.img_size,))
         img = self.generator(z)
 
         # For the combined model we will only train the generator
@@ -49,12 +47,11 @@ class GAN():
         self.combined = Model(z, validity)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-
     def build_generator(self):
 
         model = Sequential()
 
-        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(Dense(256, input_dim=self.img_size))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.8))
         model.add(Dense(512))
@@ -68,10 +65,10 @@ class GAN():
 
         model.summary()
 
-        noise = Input(shape=(self.latent_dim,))
-        img = model(noise)
+        sketch = Input(shape=(self.img_size,))
+        picture = model(sketch)
 
-        return Model(noise, img)
+        return Model(sketch, picture)
 
     def build_discriminator(self):
 
@@ -85,22 +82,36 @@ class GAN():
         model.add(Dense(1, activation='sigmoid'))
         model.summary()
 
-        img = Input(shape=self.img_shape)
-        validity = model(img)
+        picture = Input(shape=self.img_shape)
+        validity = model(picture)
 
-        return Model(img, validity)
+        return Model(picture, validity)
 
     def train(self, epochs, batch_size=128, sample_interval=50):
-
         # Generate the dataset
-        generate_dataset.build("cropped_sketch", self.img_cols, self.img_rows, 1)
+        dataset.generate("sketches", self.img_cols, self.img_rows, 0.9)
+        dataset.generate("pictures", self.img_cols, self.img_rows, 0.9)
 
         # Load the dataset
-        (X_train, _) = load_dataset.get("cropped_sketch")
+        (X_train, X_test) = dataset.load("sketches")
+        (Y_train, Y_test) = dataset.load("pictures")
 
         # Rescale -1 to 1
         X_train = X_train / 127.5 - 1.
         X_train = np.expand_dims(X_train, axis=3)
+
+        X_test = X_test / 127.5 - 1.
+        X_test = np.expand_dims(X_test, axis=3)
+
+        Y_train = Y_train / 127.5 - 1.
+        Y_train = np.expand_dims(Y_train, axis=3)
+
+        Y_test = Y_test / 127.5 - 1.
+        Y_test = np.expand_dims(Y_test, axis=3)
+
+        # Reshape sketches for generator
+        X_train = X_train.reshape(X_train.shape[0], self.img_size)
+        X_test = X_test.reshape(X_test.shape[0], self.img_size)
 
         # Adversarial ground truths
         valid = np.ones((batch_size, 1))
@@ -114,15 +125,14 @@ class GAN():
 
             # Select a random batch of images
             idx = np.random.randint(0, X_train.shape[0], batch_size)
-            imgs = X_train[idx]
-
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            sketches = X_train[idx]
+            pictures = Y_train[idx]
 
             # Generate a batch of new images
-            gen_imgs = self.generator.predict(noise)
+            gen_imgs = self.generator.predict(sketches)
 
             # Train the discriminator
-            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            d_loss_real = self.discriminator.train_on_batch(pictures, valid)
             d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
@@ -130,37 +140,61 @@ class GAN():
             #  Train Generator
             # ---------------------
 
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            # Select a random batch of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            sketches = X_train[idx]
 
             # Train the generator (to have the discriminator label samples as valid)
-            g_loss = self.combined.train_on_batch(noise, valid)
+            g_loss = self.combined.train_on_batch(sketches, valid)
 
             # Plot the progress
-            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
             # If at save interval => save generated image samples
             if epoch % sample_interval == 0:
-                self.sample_images(epoch)
+                self.sample_images(epoch, X_test, Y_test)
 
-    def sample_images(self, epoch):
-        r, c = 5, 5
-        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
-        gen_imgs = self.generator.predict(noise)
+    def sample_images(self, epoch, X_test, Y_test):
+        sample_size = 32
+
+        # idx = np.random.randint(0, X_train.shape[0], sample_size)
+        # sketches = X_test[idx]
+        # pictures = Y_test[idx]
+
+        sketches = X_test[0:sample_size]
+        pictures = Y_test[0:sample_size]
+
+        gen_imgs = self.generator.predict(sketches)
 
         # Rescale images 0 - 1
         gen_imgs = 0.5 * gen_imgs + 0.5
 
-        fig, axs = plt.subplots(r, c)
-        cnt = 0
-        for i in range(r):
-            for j in range(c):
-                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
-                axs[i,j].axis('off')
-                cnt += 1
-        fig.savefig("images/%d.png" % epoch)
-        plt.close()
+        sketches = sketches.reshape(sample_size, self.img_rows, self.img_cols, self.channels)
+
+        # Make directories
+        if not os.path.exists('gan_results/pictures/' + str(epoch)):
+            os.makedirs('gan_results/pictures/' + str(epoch))
+
+        if not os.path.exists('gan_results/sketches/' + str(epoch)):
+            os.makedirs('gan_results/sketches/' + str(epoch))
+
+        if not os.path.exists('gan_results/output/' + str(epoch)):
+            os.makedirs('gan_results/output/' + str(epoch))
+
+        # Save images
+        for i in range(len(sketches)):
+            img = image.array_to_img(sketches[i])
+            img.save('gan_results/sketches/' + str(epoch) + '/' + str(i) + '.png')
+
+        for i in range(len(pictures)):
+            img = image.array_to_img(pictures[i])
+            img.save('gan_results/pictures/' + str(epoch) + '/' + str(i) + '.png')
+
+        for i in range(len(gen_imgs)):
+            img = image.array_to_img(gen_imgs[i])
+            img.save('gan_results/output/' + str(epoch) + '/' + str(i) + '.png')
 
 
 if __name__ == '__main__':
     gan = GAN()
-    gan.train(epochs=30000, batch_size=32, sample_interval=200)
+    gan.train(epochs=100000, batch_size=32, sample_interval=1000)
